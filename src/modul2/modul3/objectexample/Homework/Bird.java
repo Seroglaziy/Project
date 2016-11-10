53c82005312f830fb5ae3a7f2203105caac484c9
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-11-10 15:18:02 +0200

    Bird

---
+++ src/modul2/modul3/objectexample/Homework/Bird.java
@@ -0,0 +1,25 @@
+package modul2.modul3.objectexample.Homework;
+
+/**
+ * Created by Семья on 10.11.2016.
+ */
+public class Bird {
+
+    public static void main(String[] args){
+
+        System.out.println("I am walking");
+        System.out.println("I am flying");
+        System.out.println("I am singing");
+        System.out.println("I am Bird");
+
+
+        }
+
+    private static String sign(String[] args) {
+        return null;
+    }
+
+}
+
+
+
