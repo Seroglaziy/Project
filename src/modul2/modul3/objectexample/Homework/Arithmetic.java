99eb2497103aa412f45252dbdc66e5b5698ceffd
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-11-10 16:37:52 +0200

    Arithmetic

---
+++ src/modul2/modul3/objectexample/Homework/Arithmetic.java
@@ -0,0 +1,20 @@
+package modul2.modul3.objectexample.Homework;
+
+/**
+ * Created by Семья on 10.11.2016.
+ */
+public class Arithmetic {
+
+    public static void main(String[] args) {
+
+       int i = 10;
+    }
+
+      int add(int index ){
+         int i = 10;
+         return i;
+
+     }
+
+
+}
