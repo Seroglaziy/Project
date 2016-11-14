b8478bfd5f300d39072d6a27a848a6f6a0d0e7ff
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-11-14 17:23:36 +0200

    Student

---
+++ src/modul2/modul3/objectexample/Homework/Student.java
@@ -0,0 +1,88 @@
+package modul2.modul3.objectexample.Homework;
+
+/**
+ * Created by Семья on 14.11.2016.
+ */
+public class Student {
+    public static void main(String[] args) {
+
+    }
+
+
+
+    private String lastName;
+    private String firstName;
+    private int group;
+    private Course[] coursesTaken;
+    private int age;
+
+
+
+    public Student(String firstName, String lastName, int group, Course[] coursesTaken, int age, String collegeName, int rating, long id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.group = group;
+        this.coursesTaken = coursesTaken;
+        this.age = age;
+
+    }
+
+
+
+    public Student(String firstName, String lastName, int group) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.group = group;
+    }
+
+    public Student(String firstName, Course[] coursesTaken) {
+        this.firstName = firstName;
+        this.coursesTaken = coursesTaken;
+
+
+    }
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getGroup() {
+        return group;
+    }
+
+    public void setGroup(int group) {
+        this.group = group;
+    }
+
+    public Course[] getCoursesTaken() {
+        return coursesTaken;
+    }
+
+    public void setCoursesTaken(Course[] coursesTaken) {
+        this.coursesTaken = coursesTaken;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+
+    }
+
+
