92f24cc754beed71e51deb4b89a5da70276196ea
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-11-16 16:03:49 +0200

    Adder

---
+++ src/modul2/modul3/objectexample/Homework/Task3/Task2/Adder.java
@@ -0,0 +1,28 @@
+package modul2.modul3.objectexample.Homework.Task3.Task2;
+
+/**
+ * Created by Семья on 10.11.2016.
+ */
+public class Adder {
+    public static void main(String[] args) {
+
+    }
+
+
+    public boolean check(int a, int b) {
+        if (a >= b)
+            return true;
+        else return false;
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
