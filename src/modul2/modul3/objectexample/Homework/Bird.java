88dba0c6658e7d8a8eb63484db9a218214ea5e85
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-11-16 21:27:22 +0200

    Bird

--- src/modul2/modul3/objectexample/Homework/Bird.java
+++ src/modul2/modul3/objectexample/Homework/Bird.java
@@ -1,25 +1,34 @@
 package modul2.modul3.objectexample.Homework;
 
+import static java.awt.SystemColor.text;
+
 /**
  * Created by Семья on 10.11.2016.
  */
-public class Bird {
+public class Bird{
+    public static void main(String[] args) {
+        sing();
 
-    public static void main(String[] args){
-
+    }
+    private static void sing() {
         System.out.println("I am walking");
         System.out.println("I am flying");
         System.out.println("I am singing");
         System.out.println("I am Bird");
-
-
-        }
-
-    private static String sign(String[] args) {
-        return null;
     }
 
+
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
