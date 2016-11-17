8fba8339463098fe733b4a07aed03cefe9a7e955
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-11-17 18:20:46 +0200

    Adder

--- src/modul2/modul3/objectexample/Homework/Task3/Task2/Adder.java
+++ src/modul2/modul3/objectexample/Homework/Task3/Task2/Adder.java
@@ -3,20 +3,24 @@ package modul2.modul3.objectexample.Homework.Task3.Task2;
 /**
  * Created by Семья on 10.11.2016.
  */
-public class Adder {
+public class Adder extends Arithmetic {
     public static void main(String[] args) {
 
     }
 
 
     public boolean check(int a, int b) {
+        a = 20;
+        b = 10;
+        int c = a + b;
         if (a >= b)
-            return true;
-        else return false;
+        return true;
+       else return false;
+
+
 
     }
 }
-
 
 
 
