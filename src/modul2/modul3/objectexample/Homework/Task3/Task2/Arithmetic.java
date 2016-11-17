7b859a20a731d98d3d9b46c8edb1bdd565b5bdec
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-11-17 19:22:39 +0200

    Arithmetic

--- src/modul2/modul3/objectexample/Homework/Task3/Task2/Arithmetic.java
+++ src/modul2/modul3/objectexample/Homework/Task3/Task2/Arithmetic.java
@@ -4,14 +4,25 @@ package modul2.modul3.objectexample.Homework.Task3.Task2;
  * Created by Семья on 10.11.2016.
  */
 public class Arithmetic {
+    public static void main(String[] args) {
+        Arithmetic arr = new Arithmetic();
+        Adder add = new Adder();
+        System.out.println(add.add(10, 15));
+        System.out.println(add.check(113, 15));
+    }
 
-
-    public int add(int[] ints){
-        int sum = 10;
-        for (int var : ints) {
-            sum += var;
-        }
+    public Integer add(Integer a, Integer b) {
+        Integer sum = a + b;
         return sum;
+
+
+
+    //public int add(int[] ints){
+        //int sum = 10;
+        //for (int var : ints) {
+           // sum += var;
+        //}
+        //return sum;
     }
 }
 
