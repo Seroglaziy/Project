7f2ed6e8189f3934cc0b9f5d20c689e63eae7c2b
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-11-18 20:52:02 +0200

    Main

---
+++ src/modul2/modul3/objectexample/Homework/Task4/Main.java
@@ -0,0 +1,17 @@
+package modul2.modul3.objectexample.Homework.Task4;
+
+/**
+ * Created by Семья on 18.11.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        User user1 = new User("Vadim", 5000, 6, "Luxoft", 2000, "usd");
+        user1.paySalary(2000, 5000);
+        System.out.println("user1.withdraw" +(1000));
+        user1.monthIncreaser(2);
+        System.out.println(user1.getBalance());
+    }
+
+    }
+
