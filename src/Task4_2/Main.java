b8d39bcaf1f8781bc6f523240b2d0116c5063939
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-11-19 15:52:53 +0200

    Main

---
+++ src/Task4_2/Main.java
@@ -0,0 +1,18 @@
+package Task4_2;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import static Task4_2.Currency.EUR;
+import static Task4_2.Currency.USD;
+
+/**
+ * Created by Семья on 19.11.2016.
+ */
+public class Main{
+    public static void main(String[] args) {
+        System.out.println(USD);
+        System.out.println(EUR);
+
+
+    }
+}
