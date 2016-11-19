e885f2fb09ea598d22f2570a45a1e03ff25b1e9a
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-11-19 14:30:22 +0200

    ChinaBank

--- src/modul4/ChinaBank.java
+++ src/modul4/ChinaBank.java
@@ -5,8 +5,11 @@ import java.util.Currency;
 /**
  * Created by Семья on 19.11.2016.
  */
-public abstract class ChinaBank {
+public abstract class ChinaBank extends Bank {
 
+    public ChinaBank(long id, String bankCountry, Currency currency, int numberOfEmployees, double avrSalaryOfEmployee, long rating, long totalCapital) {
+        super(id, bankCountry, currency, numberOfEmployees, avrSalaryOfEmployee, rating, totalCapital);
+    }
 }
 
 
