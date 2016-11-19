0523fcf732b9e8feacdae2b70347728c22f27070
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-11-19 14:30:35 +0200

    EUBank

--- src/modul4/EUBank.java
+++ src/modul4/EUBank.java
@@ -5,7 +5,10 @@ import java.util.Currency;
 /**
  * Created by Семья on 19.11.2016.
  */
-public abstract class EUBank {
+public abstract class EUBank extends Bank {
 
+    public EUBank(long id, String bankCountry, Currency currency, int numberOfEmployees, double avrSalaryOfEmployee, long rating, long totalCapital) {
+        super(id, bankCountry, currency, numberOfEmployees, avrSalaryOfEmployee, rating, totalCapital);
     }
+}
 
