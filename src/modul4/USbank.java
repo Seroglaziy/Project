781c54a3cbc3c23e8667e6a7381504bf1ec3898e
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-11-19 14:30:55 +0200

    USbank

--- src/modul4/USbank.java
+++ src/modul4/USbank.java
@@ -7,7 +7,10 @@ import java.util.Currency;
 /**
  * Created by Семья on 19.11.2016.
  */
-public abstract class USbank {
+public abstract class USbank extends Bank{
 
 
+    public USbank(long id, String bankCountry, Currency currency, int numberOfEmployees, double avrSalaryOfEmployee, long rating, long totalCapital) {
+        super(id, bankCountry, currency, numberOfEmployees, avrSalaryOfEmployee, rating, totalCapital);
+    }
 }
