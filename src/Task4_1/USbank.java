53a78d54cb6bed853a19e25e34b2aaa6891cbfcd
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-11-19 15:56:51 +0200

    USbank

--- src/Task4_1/USbank.java
+++ src/Task4_1/USbank.java
@@ -1,13 +1,11 @@
-package modul4;
-
-import modul2.practic.*;
+package Task4_1;
 
 import java.util.Currency;
 
 /**
  * Created by Семья on 19.11.2016.
  */
-public abstract class USbank extends Bank{
+public abstract class USbank extends Task4_1.Bank {
 
 
     public USbank(long id, String bankCountry, Currency currency, int numberOfEmployees, double avrSalaryOfEmployee, long rating, long totalCapital) {
