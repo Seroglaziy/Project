1c658fdb90d416a78e4857d4b5176ab5495f81c3
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-11-20 20:36:33 +0200

    USBank

--- src/modul4/Task4_2/Task4_1/USbank.java
+++ src/modul4/Task4_2/Task4_1/USbank.java
@@ -1,14 +1,45 @@
 package modul4.Task4_2.Task4_1;
 
-import java.util.Currency;
+
+
 
 /**
  * Created by Семья on 19.11.2016.
  */
-public abstract class USbank extends modul4.Task4_2.Task4_1.Bank {
+public class USBank extends Bank {
 
 
-    public USbank(long id, String bankCountry, Currency currency, int numberOfEmployees, double avrSalaryOfEmployee, long rating, long totalCapital) {
+
+    public USBank(long id, String bankCountry, Currency currency, int numberOfEmployees, double avrSalaryOfEmployee, long rating, long totalCapital) {
         super(id, bankCountry, currency, numberOfEmployees, avrSalaryOfEmployee, rating, totalCapital);
+    }
+
+    @Override
+    int getLimitOfWithdrawal() {
+        if(this.getCurrency()==Currency.USD) return 1000;
+        else return 1200;
+    }
+
+    @Override
+    int getLimitOfFunding() {
+        if(this.getCurrency()==Currency.EUR) return 10000;
+        else return 0;
+    }
+
+    @Override
+    int getMonthlyRate() {
+        if(this.getCurrency()==Currency.EUR) return 1;
+        return 1;
+    }
+    @Override
+    public int getCommission(int summ) {
+        if (this.getCurrency()==Currency.USD){
+            if (summ<1000) return 5;
+            else return 7;
+        }
+        else {
+            if (summ<1000) return 6;
+            else return 8;
+        }
     }
 }
