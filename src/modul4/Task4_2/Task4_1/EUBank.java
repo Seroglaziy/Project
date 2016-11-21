7d5be8a10e53361e31eb6f6df5b77faf47d863c7
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-11-21 13:37:43 +0200

    EUBank

--- src/modul4/Task4_2/Task4_1/EUBank.java
+++ src/modul4/Task4_2/Task4_1/EUBank.java
@@ -1,14 +1,45 @@
 package modul4.Task4_2.Task4_1;
 
-import java.util.Currency;
+import modul4.Task4_2.Currency;
+import modul4.Task4_2.Task4_1.Bank;
 
 /**
- * Created by Семья on 19.11.2016.
+ * Created by Семья on 21.11.2016.
  */
-public abstract class EUBank extends Bank {
+public class EUBank extends Bank {
+
 
     public EUBank(long id, String bankCountry, Currency currency, int numberOfEmployees, double avrSalaryOfEmployee, long rating, long totalCapital) {
         super(id, bankCountry, currency, numberOfEmployees, avrSalaryOfEmployee, rating, totalCapital);
     }
-}
 
+    @Override
+    int getLimitOfWithdrawal() {
+        if (this.getCurrency() == Currency.USD) return 2000;
+        else return 2200;
+    }
+
+    @Override
+    int getLimitOfFunding() {
+        if (this.getCurrency() == Currency.EUR) return 20000;
+        else return 10000;
+    }
+
+    @Override
+    int getMonthlyRate() {
+        if (this.getCurrency() == Currency.USD) return 0;
+        else return 1;
+    }
+
+    @Override
+    int getCommission(int amount) {
+        if (this.getCurrency() == Currency.USD) {
+            if (amount < 1000) return 5;
+            else return 7;
+        }
+         else{
+            if (amount < 1000) return 2;
+            else return 4;
+        }
+    }
+}
