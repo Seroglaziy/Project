b16cbaec0058a68ca4ad2909eef8d079daac5794
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-11-21 13:37:32 +0200

    ChinaBank

--- src/modul4/Task4_2/Task4_1/ChinaBank.java
+++ src/modul4/Task4_2/Task4_1/ChinaBank.java
@@ -1,17 +1,44 @@
 package modul4.Task4_2.Task4_1;
 
-import java.util.Currency;
+import modul4.Task4_2.Currency;
+import modul4.Task4_2.Task4_1.Bank;
 
 /**
- * Created by Семья on 19.11.2016.
+ * Created by Семья on 21.11.2016.
  */
-public abstract class ChinaBank extends Bank {
+public class ChinaBank extends Bank {
 
     public ChinaBank(long id, String bankCountry, Currency currency, int numberOfEmployees, double avrSalaryOfEmployee, long rating, long totalCapital) {
         super(id, bankCountry, currency, numberOfEmployees, avrSalaryOfEmployee, rating, totalCapital);
     }
+
+    @Override
+    int getLimitOfWithdrawal() {
+        if (this.getCurrency() == Currency.USD) return 100;
+        else return 150;
+    }
+
+    @Override
+    int getLimitOfFunding() {
+        if (this.getCurrency() == Currency.EUR) return 5000;
+        else return 10000;
+    }
+
+    @Override
+    int getMonthlyRate() {
+        if (this.getCurrency() == Currency.USD) return -1;
+        else return 0;
+    }
+
+    @Override
+    int getCommission(int amount) {
+        if (this.getCurrency() == Currency.USD){
+            if (amount < 1000) return 3;
+            else return 5;
+        }
+        else {
+            if(amount<1000) return 10;
+            else return 11;
+        }
+    }
 }
-
-
-
-
