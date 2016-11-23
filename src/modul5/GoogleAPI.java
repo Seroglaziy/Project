475b6c1651c6c19c2510e244558602c0af67a99f
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-11-23 20:31:43 +0200

    GoogleAPI

--- src/modul5/GoogleAPI.java
+++ src/modul5/GoogleAPI.java
@@ -1,18 +1,23 @@
 package modul5;
 
+import java.util.Date;
+
 /**
  * Created by Семья on 23.11.2016.
  */
 public  class GoogleAPI implements API {
 
 
-    public GoogleAPI(Room[]rooms){
-        this.rooms = rooms;
+    public GoogleAPI() {
+        Date date = new Date();
+        Room[] rooms = new Room[5];
+        rooms[0] = new Room(250 , 350 , 450 , date , "Mirrors Design Hotel" , "Киев");
+        rooms[1] = new Room(350 , 450 ,550 , date , "Салют" , "Киев");
+        rooms[2] = new Room(450 , 550 , 650 , date , "Знание", "Киев");
+        rooms[3] = new Room(550, 650 , 750 , date ,"Сити Парк Отель" , "Киев");
+        rooms[4] = new Room(650, 750 , 850 , date ,"Отель Бонтиак" , "Киев");
+
     }
-
-    Room[] rooms = new Room[5];
-
-
     @Override
     public Room[] findRooms(int price, int persons, String city, String hotel) {
         return new Room[0];
