8633ebbc964c9ff7e40f92f6262ad3877af1c0d5
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-11-23 20:31:56 +0200

    TripAdvisorAPI

--- src/modul5/TripAdvisorAPI.java
+++ src/modul5/TripAdvisorAPI.java
@@ -1,20 +1,24 @@
 package modul5;
 
 import java.math.RoundingMode;
+import java.util.Date;
 
 /**
  * Created by Семья on 23.11.2016.
  */
 public class TripAdvisorAPI implements API {
 
+    public TripAdvisorAPI() {
 
-    public TripAdvisorAPI(Room[]rooms){
-        this.rooms = rooms;
+        Date date = new Date();
+        Room[] rooms = new Room[5];
+        rooms[0] = new Room(150 , 250 , 350 , date , "Сапфир" , "Киев");
+        rooms[1] = new Room(250, 350, 450 , date , "Украина" , "Киев");
+        rooms[2] = new Room(350 , 450 , 550 , date , "Крещатик" , "Киев");
+        rooms[3] = new Room(450 , 550 , 650 , date , "АпартОтель", "Киев");
+        rooms[4] = new Room(325 , 425 , 525, date , "Воздвиженский" , "Киев");
 
     }
-
-    Room[] rooms = new Room[5];
-
     @Override
     public Room[] findRooms(int price, int persons, String city, String hotel) {
         return new Room[0];
