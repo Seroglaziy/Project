3eebb48837879939fff13d2a0e7b89e5bdcb801c
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-11-23 19:28:12 +0200

    DAO

---
+++ src/modul5/DAO.java
@@ -0,0 +1,12 @@
+package modul5;
+
+/**
+ * Created by Семья on 23.11.2016.
+ */
+public interface DAO  {
+
+    Room save(Room room);
+    boolean delete(Room room);
+    Room update(Room room);
+    Room findById(long id);
+}
