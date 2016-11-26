79923249a57d8abe3b934d7016b3555ab1a963ce
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-11-26 15:44:42 +0200

    USBank

--- src/modul4/Task4_2/Task4_1/Task4_3/USBank.java
+++ src/modul4/Task4_2/Task4_1/Task4_3/USBank.java
@@ -1,7 +1,5 @@
-package modul4.Task4_2.Task4_1;
+package modul4.Task4_2.Task4_1.Task4_3;
 
-
-import modul4.Task4_2.Currency;
 
 /**
  * Created by Семья on 19.11.2016.
@@ -14,32 +12,31 @@ public class USBank extends Bank {
     }
 
 
-
     @Override
     int getLimitOfWithdrawal() {
-        if(this.getCurrency()== Currency.USD) return 1000;
+        if (this.getCurrency() == Currency.USD) return 1000;
         else return 1200;
     }
 
     @Override
     int getLimitOfFunding() {
-        if(this.getCurrency()==Currency.EUR) return 10000;
+        if (this.getCurrency() == Currency.EUR) return 10000;
         else return 0;
     }
 
     @Override
     int getMonthlyRate() {
-        if(this.getCurrency()==Currency.EUR) return -1;
-         else return 2;
+        if (this.getCurrency() == Currency.USD) return -1;
+        else return 2;
     }
+
     @Override
     public int getCommission(int summ) {
-        if (this.getCurrency()==Currency.USD){
-            if (summ<1000) return 5;
+        if (this.getCurrency() == Currency.USD) {
+            if (summ < 1000) return 5;
             else return 7;
-        }
-        else {
-            if (summ<1000) return 6;
+        } else {
+            if (summ < 1000) return 6;
             else return 8;
         }
     }
