8f06f39c0bd1a7edda5c6ea116a4e176f58d24ed
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-11-26 19:23:48 +0200

    BookingComAPI

--- src/modul5/BookingComAPI.java
+++ src/modul5/BookingComAPI.java
@@ -2,21 +2,24 @@ package modul5;
 
 import java.util.Date;
 
+
+
+
 /**
  * Created by Семья on 23.11.2016.
  */
 public class BookingComAPI implements API {
 
+    public Room[] rooms;
+
     public BookingComAPI() {
-
-
-        Room[] rooms = new Room[5];
+        rooms = new Room[5];
         Date date = new Date();
         rooms[0] = new Room(100, 1000, 100, date, "Братислава", "Киев");
-        rooms[1] = new Room(200 ,1500 , 200 , date , "Premier Palace" , "Киев");
-        rooms[2] = new Room(300 , 1800, 300 , date , "Днепр", "Киев");
-        rooms[3] = new Room(400 , 2000 , 400, date , "Турис", "Киев");
-        rooms[4] = new Room(500 , 2200 , 500 , date, "Славутич" , "Киев");
+        rooms[1] = new Room(200, 1500, 200, date, "Premier Palace", "Киев");
+        rooms[2] = new Room(300, 1800, 300, date, "Днепр", "Киев");
+        rooms[3] = new Room(400, 2000, 400, date, "Турис", "Киев");
+        rooms[4] = new Room(500, 2200, 500, date, "Славутич", "Киев");
 
     }
 
