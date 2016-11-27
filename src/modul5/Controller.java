32adff481c20695a99b23946c5e1d2e4d260db3c
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-11-27 16:43:27 +0200

    Controller

---
+++ src/modul5/Controller.java
@@ -0,0 +1,58 @@
+package modul5;
+
+
+
+/**
+ * Created by Семья on 26.11.2016.
+ */
+public class Controller {
+    public static void main(String[] args) {
+
+    }
+
+
+    API apis[] = {new BookingComAPI(), new TripAdvisorAPI(), new GoogleAPI()};
+
+
+    Room[] requstRooms(int price, int persons, String city, String hotel) {
+        int count = 0;
+        for (API a : apis) {
+            count += a.findRooms(price, persons, city, hotel).length;
+        }
+        Room[] res = new Room[count];
+        int i = 0;
+        for (API a : apis) {
+            for (Room r : a.findRooms(price, persons, city, hotel))
+                res[i] = r;
+            i++;
+        }
+        return res;
+
+    }
+
+    Room[] check(API api1, API api2) {
+        int count = 0;
+        for (int i = 0; i < api1.getRooms().length; i++) {
+            for (int j = 0; j < api1.getRooms().length; j++) {
+                if (api1.getRooms()[i].equals(api2.getRooms()[j])) {
+                    count++;
+                }
+            }
+        }
+        Room[] res = new Room[count];
+        for (int i = 0; i < api1.getRooms().length; i++) {
+            if (api1.getRooms()[i].equals(api2.getRooms()[i])) {
+                res[i] = api1.getRooms()[i];
+            }
+        }
+        return res;
+    }
+
+}
+
+
+
+
+
+
+
