af5bd450eecc5aa746dc23bf241fb088af74e0bc
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-12-01 21:37:48 +0200

    BookingComAPI

--- src/modul5/BookingComAPI.java
+++ src/modul5/BookingComAPI.java
@@ -1,42 +1,40 @@
 package modul5;
 
-import java.sql.Connection;
 import java.util.Date;
 
-
-
-
-/**
- * Created by Семья on 23.11.2016.
- */
 public class BookingComAPI implements API {
-
     public Room[] rooms;
 
+
     public BookingComAPI() {
-        rooms = new Room[5];
         Date date = new Date();
-        rooms[0] = new Room(100, 1000, 100, date, "Братислава", "Киев");
-        rooms[1] = new Room(200, 1500, 200, date, "Premier Palace", "Киев");
-        rooms[2] = new Room(300, 1800, 300, date, "Днепр", "Киев");
-        rooms[3] = new Room(400, 2000, 400, date, "Турис", "Киев");
-        rooms[4] = new Room(500, 2200, 500, date, "Славутич", "Киев");
+        rooms = new Room[5];
+        rooms[0] = new Room(963852741, 150, 1, date, "The Cosmopolitan", "Лас-Вегас");
+        rooms[1] = new Room(365552, 250, 2, date, "Burj Al Arab", "Дубай");
+        rooms[2] = new Room(36012, 150, 2, date, "KharkivPalace", "Kharkiv");
+        rooms[3] = new Room(126, 150, 2, date, "Hilton", "New York");
+        rooms[4] = new Room(22543, 150, 2, date, "Universal's Cabana Bay Beach Resort", "Орландо");
 
     }
-
-
-
-
 
     @Override
     public Room[] findRooms(int price, int persons, String city, String hotel) {
-    return new Room[0];
-}
+        Room[] booking = new Room[0];
+        Date date=new Date();
+        Room compareRoom= new Room(236544,price,persons,date,hotel,city);
+        for (int i = 0; i < rooms.length; i++) {
+            if (rooms[i].equals(compareRoom)){
+                Room[] Same1 = new Room[booking.length + 1];
+                System.arraycopy(booking, 0, Same1, 0, booking.length);
+                booking = Same1;
+                booking[booking.length - 1] = rooms[i];
+            }
+        }
+        return booking;
+    }
 
     @Override
-    public Room[] getRooms() {
-        return rooms;
+    public Room[] getAll() {
+        return this.rooms;
     }
 }
-
-
