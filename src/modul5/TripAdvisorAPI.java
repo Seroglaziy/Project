d829d23bb641bbaa68753364aecd04138ffa549b
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-12-01 21:40:07 +0200

    TripAdvisorAPI

--- src/modul5/TripAdvisorAPI.java
+++ src/modul5/TripAdvisorAPI.java
@@ -1,33 +1,39 @@
 package modul5;
 
-import java.math.RoundingMode;
 import java.util.Date;
 
-/**
- * Created by Семья on 23.11.2016.
- */
 public class TripAdvisorAPI implements API {
-
     public Room[]rooms;
 
     public TripAdvisorAPI() {
-        rooms = new Room[5];
         Date date = new Date();
-        rooms[0] = new Room(150 , 250 , 350 , date , "Сапфир" , "Киев");
-        rooms[1] = new Room(250, 350, 450 , date , "Украина" , "Киев");
-        rooms[2] = new Room(350 , 450 , 550 , date , "Крещатик" , "Киев");
-        rooms[3] = new Room(450 , 550 , 650 , date , "АпартОтель", "Киев");
-        rooms[4] = new Room(325 , 425 , 525, date , "Воздвиженский" , "Киев");
+        rooms= new Room[5];
+        rooms[0] = new Room(126, 150, 2, date, "Hilton", "New York");
+        rooms[1] = new Room(963852741, 150, 1, date, "The Cosmopolitan", "Лас-Вегас");
+        rooms[2] = new Room(100241, 200, 1, date, "Drury Plaza ", "Сан-Антонио");
+        rooms[3] = new Room(22543, 150, 2, date, "Universal's Cabana Bay Beach Resort", "Орландо");
+        rooms[4] = new Room(365552, 250, 2, date, "Burj Al Arab", "Дубай");
 
     }
+
     @Override
     public Room[] findRooms(int price, int persons, String city, String hotel) {
-        return new Room[0];
+        Room[] trip = new Room[0];
+        Date d=new Date();
+        Room comparesRoom= new Room(236544,price,persons,d,hotel,city);
+        for (int i = 0; i < rooms.length; i++) {
+            if (rooms[i].equals(comparesRoom)){
+                Room[] Same2 = new Room[trip.length + 1];
+                System.arraycopy(trip, 0, Same2, 0, trip.length);
+                trip = Same2;
+                trip[trip.length - 1] = rooms[i];
+            }
+        }
+        return trip;
     }
 
     @Override
-    public Room[] getRooms() {
-        return rooms;
+    public Room[] getAll() {
+        return this.rooms;
     }
 }
-
