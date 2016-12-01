ea4f83ce10635a6eb5bc4b0b4a7414a9936f0814
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-12-01 21:37:33 +0200

    API

--- src/modul5/API.java
+++ src/modul5/API.java
@@ -1,6 +1,6 @@
 package modul5;
 
-import java.sql.Connection;
+
 
 /**
  * Created by Семья on 23.11.2016.
@@ -10,7 +10,8 @@ public interface API {
 
      Room[] findRooms(int price, int persons, String city, String hotel);
 
-     Room[] getRooms();
+     Room[] getAll();
+
 
 
 }
