16455a07a0fa9af821210b8cc02be8eb650972a3
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-12-01 21:43:01 +0200

    DAOimpl

--- src/modul5/DAOimpl.java
+++ src/modul5/DAOimpl.java
@@ -1,33 +1,85 @@
 package modul5;
 
-import java.util.Date;
+public class DAOimpl implements DAO{
 
-/**
- * Created by Семья on 27.11.2016.
- */
-public class DAOimpl implements DAO {
+    private Room[] DataBase = new Room[10];
+
+    public void setDataBase(Room dataBase) {
+        this.save(dataBase);
+    }
+
+    public Room[] getDataBase() {
+        return this.DataBase;
+    }
+
     @Override
     public Room save(Room room) {
-        System.out.println("Room "+room.getId()+" is saved");
-        return room;
+
+        for (int i = 0; i < DataBase.length; i++) {
+
+            if (DataBase[i] == null) {
+                DataBase[i] = room;
+                break;
+
+            }
+
+        }
+        System.out.println(room.getHotelName() + " был сохранен в Базе Данных");
+        return null;
     }
 
     @Override
     public boolean delete(Room room) {
-        System.out.println("Room " +room+" is deleted");
-        return true;
+        for (int i = 0; i < DataBase.length; i++) {
+            if (DataBase[i].equals(room)) {
+                DataBase[i] = null;
+                System.out.println(room.getHotelName() + " был удален из Базы Данных");
+                break;
+            }
+
+        }
+
+        return false;
     }
 
     @Override
     public Room update(Room room) {
-        System.out.println("Room "+room+" is updated");
-        return room;
+
+        for (int i = 0; i < DataBase.length; i++) {
+            if (DataBase[i] == null) {
+                continue;
+            } else if (DataBase[i].getId() == room.getId()) {
+                DataBase[i] = room;
+                System.out.println(room.getHotelName() + " обновлено");
+                break;
+            }
+
+        }
+        return null;
     }
 
     @Override
     public Room findById(long id) {
-        Room room = new Room(20, 300, 2,new Date(),"hotel", "city");
-        System.out.println("Searching room by id number :"+id);
-        return room;
+
+        for (int i = 0; i < DataBase.length; i++) {
+            if (DataBase[i] == null) {
+                continue;
+            } else if (DataBase[i].getId() == id) {
+                System.out.println("Результат поиска: " + DataBase[i].getHotelName());
+
+                return DataBase[i];
+            }
+        }
+
+        System.out.println("Поиск не дал результатов");
+
+        return null;
+    }
+
+
+    @Override
+    public Room[] getAll() {
+
+        return DataBase;
     }
 }
