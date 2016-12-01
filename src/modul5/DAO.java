50444b86365202ae72731b9dd3945872d6175e23
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-12-01 21:38:47 +0200

    DAO

--- src/modul5/DAO.java
+++ src/modul5/DAO.java
@@ -5,8 +5,16 @@ package modul5;
  */
 public interface DAO  {
 
+
+
+
+
     Room save(Room room);
     boolean delete(Room room);
     Room update(Room room);
     Room findById(long id);
+    Room[] getAll();
+
 }
+
+
