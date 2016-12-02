d90faaa9af5fed700412a65d6d24d30b3729f04c
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-12-02 18:40:46 +0200

    ArraysUtils

--- src/modul6/ArraysUtils.java
+++ src/modul6/ArraysUtils.java
@@ -1,5 +1,7 @@
 package modul6;
 
+import java.util.Arrays;
+
 /**
  * Created by Семья on 02.12.2016.
  */
@@ -11,13 +13,15 @@ public class ArraysUtils {
         int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
 
-       System.out.println(sum(array));
-       System.out.println(min(array));
-       System.out.println(max(array));
-       System.out.println(maxPositive(array));
-       System.out.println(multiplication(array));
-       System.out.println(modulus(array));
-       System.out.println(secondLargest(array));
+        System.out.println(sum(array));
+        System.out.println(min(array));
+        System.out.println(max(array));
+        System.out.println(maxPositive(array));
+        System.out.println(multiplication(array));
+        System.out.println(modulus(array));
+        System.out.println(secondLargest(array));
+
+
     }
 
 
@@ -26,59 +30,60 @@ public class ArraysUtils {
         for (int i = 0; i < array.length; i++)
             sum += array[i];
         return sum;
+
     }
+
 
     static int min(int array[]) {
-        int min = 0;
-        for (int i = 0; i < array.length; i++)
+        int min = array[0];
+        for (int i = 1; i < array.length; i++)
             if (array[i] < min)
                 min = array[i];
-        return min;
+        return (min);
     }
 
+
     static int max(int array[]) {
-        int max = 10;
+        int max = array[0];
         for (int j = 1; j < array.length; j++)
             if (array[j] > max)
                 max = array[j];
         return max;
 
-
     }
 
+
     static int maxPositive(int array[]) {
-        int maxPositive = 10;
+        int maxPositive = array[0];
         for (int i = 1; i < array.length; i++)
             if (array[i] > maxPositive)
                 maxPositive = array[i];
         return maxPositive;
     }
 
+
     static int multiplication(int array[]) {
-        int multiplication = 0;
-        for (int i = 1; i < array.length; i++)
+        int multiplication = array[0];
+        for (int i = 1; i < array.length; i++) {
             multiplication = multiplication * i;
+        }
         return multiplication;
     }
 
+
     static int modulus(int array[]) {
         int modulus = 10;
-        for (int i = 1; i <= array.length; i++)
+        for (int i = 1; i <= 10; i++) {
             System.out.println(modulus + "%" + i + "=" + modulus % i);
+        }
         return modulus;
     }
 
     static int secondLargest(int array[]) {
-        int secondLargest = 9;
-        for (int i = 0; i < array.length; i++)
-            secondLargest += array[i];
+        Arrays.sort(array);
+        int secondLargest = array[8];
         return secondLargest;
-
-
 
     }
 }
-
-
-
 
