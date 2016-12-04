4effa458ee6988c68c9854e6856d363078d3d486
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-12-04 13:13:20 +0200

    Homework2

--- src/modul2/practic/Home2/Homework2.java
+++ src/modul2/practic/Home2/Homework2.java
@@ -16,21 +16,21 @@ public class Homework2 {
 
     public static void main(String[] args) {
         int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        double[] array2 = new double[]{0.5, 1.3, 7, -0.5, 20.5, 13, -11.5, 17, -9.8, 31.1};
+        //double[] array2 = new double[]{0.5, 1.3, 7, -0.5, 20.5, 13, -11.5, 17, -9.8, 31.1};
 
         System.out.println(sum(array));
-        System.out.println(sum(array2));
+        //System.out.println(sum(array2));
         System.out.println(min(array));
         System.out.println(max(array));
-        System.out.println(min(array2));
-        System.out.println(max(array2));
+        //System.out.println(min(array2));
+        //System.out.println(max(array2));
         System.out.println(maxPositive(array));
-        System.out.println(maxPositive(array2));
+        //System.out.println(maxPositive(array2));
         System.out.println(multiplication(array));
-        System.out.println(multiplication(array2));
+        //System.out.println(multiplication(array2));
         System.out.println(modulus(array));
-        System.out.println(modulus(array2));
-        System.out.println(sLarg(array2));
+        //System.out.println(modulus(array2));
+        //System.out.println(sLarg(array2));
 
 
         System.out.println("sum numbers of array" + ArraysUtils.sum(array));
@@ -54,12 +54,12 @@ public class Homework2 {
         return sum;
     }
 
-    static double sum(double array2[]) {
-        double sum = 0;
-        for (int i = 0; i < array2.length; i++)
-            sum += array2[i];
-        return sum;
-    }
+   // static double sum(double array2[]) {
+   //     double sum = 0;
+   //     for (int i = 0; i < array2.length; i++)
+   //         sum += array2[i];
+   //     return sum;
+   // }
 
 
     static int min(int array[]) {
@@ -78,21 +78,21 @@ public class Homework2 {
         return max;
     }
 
-    static double min(double array2[]) {
-        double min = array2[0];
-        for (int i = 1; i < array2.length; i++)
-            if (array2[i] < min)
-                min = array2[i];
-        return (min);
-    }
+   //static double min(double array2[]) {
+   //    double min = array2[0];
+   //    for (int i = 1; i < array2.length; i++)
+   //        if (array2[i] < min)
+   //            min = array2[i];
+   //    return (min);
+   //}
 
-    static double max(double array2[]) {
-        double max = array2[0];
-        for (int i = 1; i < array2.length; i++)
-            if (array2[i] > max)
-                max = array2[i];
-        return (max);
-    }
+   //static double max(double array2[]) {
+   //    double max = array2[0];
+   //    for (int i = 1; i < array2.length; i++)
+   //        if (array2[i] > max)
+   //            max = array2[i];
+   //    return (max);
+   //}
 
     static int maxPositive(int array[]) {
         int maxPositive = array[0];
@@ -102,13 +102,13 @@ public class Homework2 {
         return (maxPositive);
     }
 
-    static double maxPositive(double array2[]) {
-        double maxPositive = array2[0];
-        for (int j = 1; j < array2.length; j++)
-            if (array2[j] > maxPositive)
-                maxPositive = array2[j];
-        return (maxPositive);
-    }
+   // static double maxPositive(double array2[]) {
+   //     double maxPositive = array2[0];
+   //     for (int j = 1; j < array2.length; j++)
+   //         if (array2[j] > maxPositive)
+   //             maxPositive = array2[j];
+   //     return (maxPositive);
+   // }
 
     static int multiplication(int array[]) {
         int multiplication = array[0];
@@ -118,13 +118,13 @@ public class Homework2 {
         return multiplication;
     }
 
-    static double multiplication(double array2[]) {
-        double multiplication = array2[0];
-        for (double i = 1; i < array2.length; i++) {
-            multiplication = multiplication * i;
-        }
-        return multiplication;
-    }
+  // static double multiplication(double array2[]) {
+  //     double multiplication = array2[0];
+  //     for (double i = 1; i < array2.length; i++) {
+  //         multiplication = multiplication * i;
+  //     }
+  //     return multiplication;
+  // }
 
     static int modulus(int array[]) {
         int modulus = 10;
@@ -134,19 +134,19 @@ public class Homework2 {
         return modulus;
     }
 
-    static double modulus(double array2[]) {
-        double modulus = 20.5;
-        for (double i = 1; i <= 20.5; i++) {
-            System.out.println(modulus + "%" + i + "=" + modulus % i);
-        }
-        return modulus;
-    }
-
-    static double sLarg(double array2[]) {
-        Arrays.sort(array2);
-        double slarg = array2[8];
-        return slarg;
-    }
+  //  static double modulus(double array2[]) {
+  //      double modulus = 20.5;
+  //      for (double i = 1; i <= 20.5; i++) {
+  //          System.out.println(modulus + "%" + i + "=" + modulus % i);
+  //      }
+  //      return modulus;
+  //  }
+//
+  //  static double sLarg(double array2[]) {
+  //      Arrays.sort(array2);
+  //      double slarg = array2[8];
+  //      return slarg;
+  //  }
 }
 
 
