ba4ac1520d7a79737156789d60ae455c1d2c3c2f
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-12-11 17:37:48 +0200

    Main

--- src/modul6/Task6_1/Task6_5/Main.java
+++ src/modul6/Task6_1/Task6_5/Main.java
@@ -1,66 +1,68 @@
 package modul6.Task6_1.Task6_5;
 
 
-import java.util.Arrays;
-
 
 /**
- * Created by Семья on 05.12.2016.
+ * Created by Семья on 02.12.2016.
  */
 public class Main {
-
-
     public static void main(String[] args) {
-        User user1 = new User(1111, "Ажимов", "Denis", 3200, 2500);
-        User user2 = new User(2222, "Vysotska", "Masha", 3200, 12000);
-        User user3 = new User(2222, "Vysotska", "Tetyana", 3200, 3500);
-        User user4 = new User(3333, "Azhymova", "Tetyana", 3200, 12000);
-        User user5 = user1;
-        User[] users = {user1, user2, user3, user4, user5};
+        User[] users = new User[10];
 
-        System.out.println("================== Работаем с массивами =================================");
-        System.out.println();
+        users[0] = new User(1, "Anna", "N", 500, 2340);
+        users[1] = new User(1, "Anna", "N", 500, 2340);
+        users[2] = new User(2, "Lena", "N", 500, 2140);
+        users[3] = null;
+        users[4] = new User(3, "Ivan", "N", 600, 340);
+        users[5] = new User(4, "Evgeniy", "N", 300, 340);
+        users[6] = new User(5, "Irina", "N", 800, 340);
+        users[7] = null;
+        users[8] = new User(6, "John", "N", 200, 20);
+        users[9] = new User(7, "Steve", "N", 500, 0);
 
-        int[] arr1 = {1, 2, 3, 4, 53, 6, 7, 8, 9, 10, 53, 2, 0, 1, 22};
-        System.out.println(Arrays.toString(arr1));
+        userArrayPrint(users);
+        System.out.println("");
+        System.out.println("==============");
+        System.out.println("");
 
-        arr1 = ArraysUtils.reverse(arr1);
-        System.out.println(Arrays.toString(arr1));
-        System.out.println(Arrays.toString(ArraysUtils.findEvenElements(arr1)));
+        for (long id : UserUtils.getUsersId(users)) {
+            System.out.println(id);
+        }
 
-        System.out.println();
-        System.out.println("================== Работаем с Юзерами =======================================");
-        System.out.println();
+        System.out.println("");
+        System.out.println("==============");
+        System.out.println("");
 
+        userArrayPrint(UserUtils.usersWithConditionalBalance(users, 340));
 
+        System.out.println("");
+        System.out.println("==============");
+        System.out.println("");
 
-        System.out.println(Arrays.toString(UserUtils.paySalaryToUsers(users)));
-        System.out.println(Arrays.toString(UserUtils.getUsersId(users)));
+        userArrayPrint(UserUtils.deleteEmptyUsers(users));
 
-        User[] uniq = UserUtils.uniqueUsers(users);
-        System.out.println(Arrays.toString(uniq));
-        System.out.println(Arrays.toString(UserUtils.usersWithContitionalBalance(uniq, 2500)));
-        User[] now = users;
-        now[4] = null;
+        System.out.println("");
+        System.out.println("==============");
+        System.out.println("");
 
+        userArrayPrint(UserUtils.paySalaryToUser(users));
 
+        System.out.println("");
+        System.out.println("==============");
+        System.out.println("");
 
-
-        System.out.println();
-        System.out.println("-----------------------------------------------------------------------------");
-        System.out.println();
-
-
-        System.out.println(Arrays.toString(now));
-       // System.out.println(Arrays.toString(UserUtils.deleteEmptyUsers(now)));
-        System.out.println(Arrays.toString(UserUtils.deleteEmptyUser(users)));
-
-    }
-
-
-
+        userArrayPrint(UserUtils.uniqueUsers(users));
 
     }
 
+    private static void userArrayPrint(User[] array) {
+        for (User user : array) {
+            if (user != null) {
+                System.out.println(user.toString());
+            } else {
+                System.out.println("NULL");
+            }
 
-
+        }
+    }
+}
