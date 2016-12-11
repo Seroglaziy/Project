c863cdafdd24a475c07afa8abdd129009c370215
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-12-11 14:08:20 +0200

    Main

--- src/modul6/Task6_1/Task6_5/Main.java
+++ src/modul6/Task6_1/Task6_5/Main.java
@@ -1,6 +1,8 @@
 package modul6.Task6_1.Task6_5;
 
+
 import java.util.Arrays;
+
 
 /**
  * Created by Семья on 05.12.2016.
@@ -9,10 +11,9 @@ public class Main {
 
 
     public static void main(String[] args) {
-
-        User user1 = new User(1111, "Timchenko", "Vadim", 3200, 2500);
-        User user2 = new User(2222, "Kayte", "Biluchevskaya", 3200, 12000);
-        User user3 = new User(2222, "Stepanenko", "Julia", 3200, 3500);
+        User user1 = new User(1111, "Ажимов", "Denis", 3200, 2500);
+        User user2 = new User(2222, "Vysotska", "Masha", 3200, 12000);
+        User user3 = new User(2222, "Vysotska", "Tetyana", 3200, 3500);
         User user4 = new User(3333, "Azhymova", "Tetyana", 3200, 12000);
         User user5 = user1;
         User[] users = {user1, user2, user3, user4, user5};
@@ -23,7 +24,6 @@ public class Main {
         int[] arr1 = {1, 2, 3, 4, 53, 6, 7, 8, 9, 10, 53, 2, 0, 1, 22};
         System.out.println(Arrays.toString(arr1));
 
-        
         arr1 = ArraysUtils.reverse(arr1);
         System.out.println(Arrays.toString(arr1));
         System.out.println(Arrays.toString(ArraysUtils.findEvenElements(arr1)));
@@ -32,21 +32,35 @@ public class Main {
         System.out.println("================== Работаем с Юзерами =======================================");
         System.out.println();
 
-        UserUtils us = new UserUtils();
-        User[] uniq = us.uniqueUsers(users);
+
+
+        System.out.println(Arrays.toString(UserUtils.paySalaryToUsers(users)));
+        System.out.println(Arrays.toString(UserUtils.getUsersId(users)));
+
+        User[] uniq = UserUtils.uniqueUsers(users);
         System.out.println(Arrays.toString(uniq));
-        System.out.println(Arrays.toString(us.usersWithContitionalBalance(uniq, 2500)));
+        System.out.println(Arrays.toString(UserUtils.usersWithContitionalBalance(uniq, 2500)));
         User[] now = users;
         now[4] = null;
+
+
+
 
         System.out.println();
         System.out.println("-----------------------------------------------------------------------------");
         System.out.println();
 
+
         System.out.println(Arrays.toString(now));
-        System.out.println(Arrays.toString(us.deleteEmptyUsers(now)));
+       // System.out.println(Arrays.toString(UserUtils.deleteEmptyUsers(now)));
+        System.out.println(Arrays.toString(UserUtils.deleteEmptyUser(users)));
 
     }
 
 
-}
+
+
+    }
+
+
+
