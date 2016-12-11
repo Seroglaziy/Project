bbeb447e14965dd72a2b801d15e1f0d5c8a553aa
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-12-11 17:37:58 +0200

    User

--- src/modul6/Task6_1/Task6_5/User.java
+++ src/modul6/Task6_1/Task6_5/User.java
@@ -6,13 +6,11 @@ package modul6.Task6_1.Task6_5;
 public class User {
 
 
-
     private long id;
     private String firstName;
     private String lastName;
     private int salary;
     private int balance;
-
 
     public User(long id, String firstName, String lastName, int salary, int balance) {
         this.id = id;
@@ -20,31 +18,6 @@ public class User {
         this.lastName = lastName;
         this.salary = salary;
         this.balance = balance;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        User user = (User) o;
-
-        if (id != user.id) return false;
-        if (salary != user.salary) return false;
-        if (balance != user.balance) return false;
-        if (firstName != null ? !firstName.equals(user.firstName) : user.firstName != null) return false;
-        return lastName != null ? lastName.equals(user.lastName) : user.lastName == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = (int) (id ^ (id >>> 32));
-        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
-        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
-        result = 31 * result + salary;
-        result = 31 * result + balance;
-        return result;
     }
 
     public long getId() {
@@ -87,8 +60,39 @@ public class User {
         this.balance = balance;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
+        User user = (User) o;
 
+        if (salary != user.salary) return false;
+        if (balance != user.balance) return false;
+        if (firstName != null ? !firstName.equals(user.firstName) : user.firstName != null) return false;
+        return lastName != null ? lastName.equals(user.lastName) : user.lastName == null;
 
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + salary;
+        result = 31 * result + balance;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("User{");
+        sb.append("id=").append(id);
+        sb.append(", firstName='").append(firstName).append('\'');
+        sb.append(", lastName='").append(lastName).append('\'');
+        sb.append(", salary=").append(salary);
+        sb.append(", balance=").append(balance);
+        sb.append('}');
+        return sb.toString();
+    }
 }
-
