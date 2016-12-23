d6db98c2a8689022b0f1f411670c9ec85ad8c76a
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-12-23 13:20:50 +0200

    PriceUserCityComparator

---
+++ src/modul7/Compare/PriceUserCityComparator.java
@@ -0,0 +1,24 @@
+package modul7.Compare;
+
+import modul7.Order;
+
+import java.util.Comparator;
+
+/**
+ * Created by Семья on 23.12.2016.
+ */
+public class PriceUserCityComparator implements Comparator<Order> {
+
+    @Override
+    public int compare(Order o1, Order o2) {
+        int price1 = o1.getPrice();
+        int price2 = o2.getPrice();
+        String city1 = o1.getUser().getCity();
+        String city2 = o2.getUser().getCity();
+        if (price1 != price2) {
+            return price1 - price2;
+        } else {
+            return city1.compareTo(city2);
+        }
+    }
+}
