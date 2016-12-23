8d2da32b502320f82eb2586824aca15c19e01235
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-12-23 13:26:31 +0200

    User

--- src/modul7/Task1/User.java
+++ src/modul7/Task1/User.java
@@ -1,106 +1,105 @@
 package modul7.Task1;
 
 /**
- * Created by Семья on 09.12.2016.
+ * Created by Семья on 23.12.2016.
  */
 public class User {
 
-   private long id;
-   private String firstName;
-   private String lastName;
-   private String city;
-   private int balance;
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String city;
+    private int balance;
 
 
-   public User(long id, String firstName, String lastName, String city, int balance) {
-      this.id = id;
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.city = city;
-      this.balance = balance;
-   }
+    public User(long id, String firstName, String lastName, String city, int balance) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.balance = balance;
+    }
 
-   public long getId() {
-      return id;
-   }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
-   public void setId(long id) {
-      this.id = id;
-   }
+        User user = (User) o;
 
-   public String getFirstName() {
-      return firstName;
-   }
+        if (id != user.id) return false;
+        if (balance != user.balance) return false;
+        if (firstName != null ? !firstName.equals(user.firstName) : user.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(user.lastName) : user.lastName != null) return false;
+        return city != null ? city.equals(user.city) : user.city == null;
 
-   public void setFirstName(String firstName) {
-      this.firstName = firstName;
-   }
+    }
 
-   public String getLastName() {
-      return lastName;
-   }
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        result = 31 * result + balance;
+        return result;
+    }
 
-   public void setLastName(String lastName) {
-      this.lastName = lastName;
-   }
+    public long getId() {
+        return id;
+    }
 
-   public String getCity() {
-      return city;
-   }
+    public void setId(long id) {
+        this.id = id;
+    }
 
-   public void setCity(String city) {
-      this.city = city;
-   }
+    public String getFirstName() {
+        return firstName;
+    }
 
-   public int getBalance() {
-      return balance;
-   }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-   public void setBalance(int balance) {
-      this.balance = balance;
-   }
+    public String getLastName() {
+        return lastName;
+    }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-   @Override
-   public boolean equals(Object o) {
-      if (this == o) return true;
-      if (o == null || getClass() != o.getClass()) return false;
+    public String getCity() {
+        return city;
+    }
 
-      User user = (User) o;
+    public void setCity(String city) {
+        this.city = city;
+    }
 
-      if (id != user.id) return false;
-      if (balance != user.balance) return false;
-      if (firstName != null ? !firstName.equals(user.firstName) : user.firstName != null) return false;
-      if (lastName != null ? !lastName.equals(user.lastName) : user.lastName != null) return false;
-      return city != null ? city.equals(user.city) : user.city == null;
+    public int getBalance() {
+        return balance;
+    }
 
-   }
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
 
-   @Override
-   public int hashCode() {
-      int result = (int) (id ^ (id >>> 32));
-      result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
-      result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
-      result = 31 * result + (city != null ? city.hashCode() : 0);
-      result = 31 * result + balance;
-      return result;
-   }
-
-
-   @Override
-   public String toString() {
-      final StringBuffer sb = new StringBuffer("User{");
-      sb.append("id=").append(id);
-      sb.append(", firstName='").append(firstName).append('\'');
-      sb.append(", lastName='").append(lastName).append('\'');
-      sb.append(", city='").append(city).append('\'');
-      sb.append(", balance=").append(balance);
-      sb.append('}');
-      return sb.toString();
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("User{");
+        sb.append("id=").append(id);
+        sb.append(", firstName='").append(firstName).append('\'');
+        sb.append(", lastName='").append(lastName).append('\'');
+        sb.append(", city='").append(city).append('\'');
+        sb.append(", balance=").append(balance);
+        sb.append('}');
+        return sb.toString();
+    }
 
 
-   }
 }
+
 
 
 
