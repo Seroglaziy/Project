ef0c74bcc404142ab0ff5b4f703ace2b4c61d90b
Vadim Timchenko <Seroglaziy2@gmai.com>
2016-12-23 13:21:53 +0200

    Order

--- src/modul7/Order.java
+++ src/modul7/Order.java
@@ -3,122 +3,106 @@ package modul7;
 import java.util.Currency;
 
 /**
- * Created by Семья on 09.12.2016.
+ * Created by Семья on 23.12.2016.
  */
-public class Order {
-
-   private long id;
-   private int price;
-   private Currency currency;
-   private String itemName;
-   private String shopIdentificator;
-   private User user;
+    public class Order implements Comparable<Order> {
 
 
+        private int price;
+        private Currency currency;
+        private String itemName;
+        private String shopIdentificator;
+        private User user;
 
-   public Order(long id, int price, Currency currency, String itemName, String shopIdentificator, User user) {
-      this.id = id;
-      this.price = price;
-      this.currency = currency;
-      this.itemName = itemName;
-      this.shopIdentificator = shopIdentificator;
-      this.user = user;
+        public Order(int price, Currency currency, String itemName, String shopIdentificator, User user) {
+            this.price = price;
+            this.currency = currency;
+            this.itemName = itemName;
+            this.shopIdentificator = shopIdentificator;
+            this.user = user;
+        }
 
-   }
+        public int getPrice() {
+            return price;
+        }
 
+        public void setPrice(int price) {
+            this.price = price;
+        }
 
+        public Currency getCurrency() {
+            return currency;
+        }
 
+        public void setCurrency(Currency currency) {
+            this.currency = currency;
+        }
 
+        public String getItemName() {
+            return itemName;
+        }
 
-   public long getId() {
-      return id;
-   }
+        public void setItemName(String itemName) {
+            this.itemName = itemName;
+        }
 
-   public void setId(long id) {
-      this.id = id;
-   }
+        public String getShopIdentificator() {
+            return shopIdentificator;
+        }
 
-   public int getPrice() {
-      return price;
-   }
+        public void setShopIdentificator(String shopIdentificator) {
+            this.shopIdentificator = shopIdentificator;
+        }
 
-   public void setPrice(int price) {
-      this.price = price;
-   }
+        public User getUser() {
+            return user;
+        }
 
-   public Currency getCurrency() {
-      return currency;
-   }
+        public void setUser(User user) {
+            this.user = user;
+        }
 
-   public void setCurrency(Currency currency) {
-      this.currency = currency;
-   }
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
 
-   public String getItemName() {
-      return itemName;
-   }
+            Order order = (Order) o;
 
-   public void setItemName(String itemName) {
-      this.itemName = itemName;
-   }
+            if (price != order.price) return false;
+            if (currency != null ? !currency.equals(order.currency) : order.currency != null) return false;
+            if (itemName != null ? !itemName.equals(order.itemName) : order.itemName != null) return false;
+            if (shopIdentificator != null ? !shopIdentificator.equals(order.shopIdentificator) : order.shopIdentificator != null)
+                return false;
+            return user != null ? user.equals(order.user) : order.user == null;
 
-   public String getShopIdentificator() {
-      return shopIdentificator;
-   }
+        }
 
-   public void setShopIdentificator(String shopIdentificator) {
-      this.shopIdentificator = shopIdentificator;
-   }
+        @Override
+        public int hashCode() {
+            int result = price;
+            result = 31 * result + (currency != null ? currency.hashCode() : 0);
+            result = 31 * result + (itemName != null ? itemName.hashCode() : 0);
+            result = 31 * result + (shopIdentificator != null ? shopIdentificator.hashCode() : 0);
+            result = 31 * result + (user != null ? user.hashCode() : 0);
+            return result;
+        }
 
-   public User getUser() {
-      return user;
-   }
+        @Override
+        public String toString() {
+            final StringBuffer sb = new StringBuffer("Order{");
+            sb.append("price=").append(price);
+            sb.append(", currency=").append(currency);
+            sb.append(", itemName='").append(itemName).append('\'');
+            sb.append(", shopIdentificator='").append(shopIdentificator).append('\'');
+            sb.append(", user=").append(user);
+            sb.append('}');
+            return sb.toString();
+        }
 
-   public void setUser(User user) {
-      this.user = user;
-   }
+        @Override
+        public int compareTo(Order o) {
+            return o.getPrice() - this.price;
+        }
 
-   @Override
-   public boolean equals(Object o) {
-      if (this == o) return true;
-      if (o == null || getClass() != o.getClass()) return false;
-
-      Order order = (Order) o;
-
-      if (id != order.id) return false;
-      if (price != order.price) return false;
-      if (currency != null ? !currency.equals(order.currency) : order.currency != null) return false;
-      if (itemName != null ? !itemName.equals(order.itemName) : order.itemName != null) return false;
-      if (shopIdentificator != null ? !shopIdentificator.equals(order.shopIdentificator) : order.shopIdentificator != null)
-         return false;
-      return user != null ? user.equals(order.user) : order.user == null;
-
-   }
-
-   @Override
-   public int hashCode() {
-      int result = (int) (id ^ (id >>> 32));
-      result = 31 * result + price;
-      result = 31 * result + (currency != null ? currency.hashCode() : 0);
-      result = 31 * result + (itemName != null ? itemName.hashCode() : 0);
-      result = 31 * result + (shopIdentificator != null ? shopIdentificator.hashCode() : 0);
-      result = 31 * result + (user != null ? user.hashCode() : 0);
-      return result;
-   }
-
-   @Override
-   public String toString() {
-      return "Order{" +
-              "id=" + id +
-              ", price=" + price +
-              ", currency=" + currency +
-              ", itemName='" + itemName + '\'' +
-              ", shopIdentificator='" + shopIdentificator + '\'' +
-              ", user=" + user +
-              '}';
-
-
-   }
 }
-
-
