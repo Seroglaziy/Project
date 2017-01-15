df0d4a93312d25edf7664a113faa62c18ffee341
Vadim Timchenko <Seroglaziy2@gmai.com>
2017-01-15 19:09:32 +0200

    Main

--- src/modul10/Task_1/Main.java
+++ src/modul10/Task_1/Main.java
@@ -3,22 +3,23 @@ package modul10.Task_1;
 /**
  * Created by Семья on 14.01.2017.
  */
-public class Main{
-    public static void main(String[] args) {
+public class Main {
+    public static void main(String[] args){
 
 
-        String one = args[2];
+
 
 
         try {
-          if (args[2] == one) throw new RuntimeException();
-         } catch (Exception e) {
-            System.out.println("Exception");
+            throw new Exception("My Exception");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
 
 
-        }finally {
+        } finally {
             System.out.println("finally is completed");
         }
-
     }
+
 }
+
