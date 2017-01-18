ded4a84c9b9b58e47441902d534fd99ac7c077fd
Vadim Timchenko <Seroglaziy2@gmai.com>
2017-01-18 22:40:23 +0200

    PriceComparator

---
+++ src/modul7/Task1/Compare/PriceComparator.java
@@ -0,0 +1,19 @@
+package modul7.Task1.Compare;
+
+
+
+import modul7.Task1.entities.Order;
+
+import java.util.Comparator;
+
+/**
+ * Created by Семья on 18.01.2017.
+ */
+public class PriceComparator implements Comparator<Order> {
+    @Override
+    public int compare(Order o1, Order o2) {
+        int price1 = o1.getPrice();
+        int price2 = o2.getPrice();
+        return price2 - price1;
+    }
+}
