68a017a86b32adc3c0f8c74e340faf2a008502b7
Vadim Timchenko <Seroglaziy2@gmai.com>
2017-01-29 13:13:26 +0200

    API

--- src/modul5/API/API.java
+++ src/modul5/API/API.java
@@ -1,13 +1,14 @@
-package modul7.hwModul5;
+package modul5.API;
 
-
-
+import modul5.entities.Room;
 
 import java.util.ArrayList;
 
 /**
- * Created by Семья on 19.01.2017.
+ * Created by Семья on 29.01.2017.
  */
 public interface API {
     ArrayList<Room> findRooms(int price, int persons, String city, String hotel);
+
+    ArrayList<Room> getAll();
 }
