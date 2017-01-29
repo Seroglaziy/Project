64acb7efd9eb0f7c7418c85ec44860590f1c99a7
Vadim Timchenko <Seroglaziy2@gmai.com>
2017-01-29 13:18:07 +0200

    User

--- src/modul7/Task1/entities/User.java
+++ src/modul7/Task1/entities/User.java
@@ -1,7 +1,9 @@
 package modul7.Task1.entities;
 
+/**
+ * Created by Семья on 29.01.2017.
+ */
 public class User {
-
     private long id;
     private String firstName;
     private String lastName;
